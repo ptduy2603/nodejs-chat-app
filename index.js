1beb74814e8825293e608d46c8ea4f336987c912
Duy Pham <duythanhpham2603@gmail.com>
2024-09-09 17:08:04 +0700

    add gitignore file

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(morgan("combined"));
+app.use("/", (req, res) => res.json({ message: "Hello user" }));
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
